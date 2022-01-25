be0c3d76dbd3bc2b2797b82154bbcfaf2f52516a
gustafssonstefan <98107329+gustafssonstefan@users.noreply.github.com>
2022-01-25 17:22:01 +0100

    Add files via upload

---
+++ src/test/java/com/codetest/mortageTest.java
@@ -0,0 +1,11 @@
+package com.codetest;
+
+import org.junit.jupiter.api.Test;
+
+public class mortageTest {
+
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+}
