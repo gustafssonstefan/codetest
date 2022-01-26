51ec88b5f72f39e28202a98eac396f67ff100467
gustafssonstefan <98107329+gustafssonstefan@users.noreply.github.com>
2022-01-26 09:32:43 +0100

    Update mortage.java

--- src/main/java/com/codetest/mortage.java
+++ src/main/java/com/codetest/mortage.java
@@ -7,6 +7,14 @@ import java.text.DecimalFormat;
 
 public class mortage {
   
+      public static float pow(float baseNum, int powNum) {
+        float result = 1;
+        for(int i = 0; i < powNum; i++) {
+            result = result * baseNum;
+        }
+        return result;
+    }
+  
     private static final int MONTHS_YEAR = 12;
     public static void main(String[] args) throws Exception {
 
@@ -39,7 +47,7 @@ float loan = Float.parseFloat(ary[1]);
 float annualInterest = Float.parseFloat(ary[2]) / 100;           //Get the value in percentage              
 int numberOfPayments = Integer.parseInt(ary[3]) * MONTHS_YEAR;   //Amount of payment in total
 float monthlyInterest =  annualInterest / MONTHS_YEAR;  
-double monthlyPayment = loan * ((monthlyInterest * (Math.pow(1 + monthlyInterest, numberOfPayments))) / ((Math.pow(1 + monthlyInterest, numberOfPayments)) -1));
+double monthlyPayment = loan * ((monthlyInterest * (pow(1 + monthlyInterest, numberOfPayments))) / ((pow(1 + monthlyInterest, numberOfPayments)) -1));
 
 DecimalFormat formatter = new DecimalFormat("###,###.00");  
 System.out.println("Name: " + name.replace("\"", "") + " ||" + " Fixed monthly payment: " + formatter.format(monthlyPayment) + " €");
